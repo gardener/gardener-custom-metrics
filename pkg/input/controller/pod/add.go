30abc9fa8ec3346bf191478686f17cbaac39834a
Ismail Alidzhikov <i.alidjikov@gmail.com>
2024-03-27 16:47:32 +0200

    Use consistent naming for the manager

--- pkg/input/controller/pod/add.go
+++ pkg/input/controller/pod/add.go
@@ -8,7 +8,7 @@ import (
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	kmgr "sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	"github.com/gardener/gardener-custom-metrics/pkg/app"
@@ -20,12 +20,12 @@ import (
 // dataRegistry is a concurrency-safe data repository where the controller finds data it needs, and stores
 // the data it produces.
 func AddToManager(
-	manager kmgr.Manager,
+	mgr manager.Manager,
 	dataRegistry scrape_target_registry.InputDataRegistry,
 	controllerOptions controller.Options,
 	log logr.Logger) error {
 
-	return gcmctl.NewControllerFactory().AddNewControllerToManager(manager, gcmctl.AddArgs{
+	return gcmctl.NewControllerFactory().AddNewControllerToManager(mgr, gcmctl.AddArgs{
 		Actuator:             NewActuator(dataRegistry, log.WithName("pod-controller")),
 		ControllerName:       app.Name + "-pod-controller",
 		ControllerOptions:    controllerOptions,
