38e2e0ba33392417cad7387a3e937ddf0b9fcc8a
Ismail Alidzhikov <i.alidjikov@gmail.com>
2024-03-27 16:22:45 +0200

    Adapt to the breaking change in the manager.Options struct

--- pkg/util/gardener/options.go
+++ pkg/util/gardener/options.go
@@ -13,6 +13,8 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 const (
@@ -142,11 +144,13 @@ func (c *ManagerConfig) Apply(opts *manager.Options) {
 	opts.LeaderElectionResourceLock = c.LeaderElectionResourceLock
 	opts.LeaderElectionID = c.LeaderElectionID
 	opts.LeaderElectionNamespace = c.LeaderElectionNamespace
-	opts.Host = c.WebhookServerHost
-	opts.Port = c.WebhookServerPort
-	opts.CertDir = c.WebhookCertDir
-	opts.MetricsBindAddress = c.MetricsBindAddress
+	opts.Metrics = metricsserver.Options{BindAddress: c.MetricsBindAddress}
 	opts.HealthProbeBindAddress = c.HealthBindAddress
+	opts.WebhookServer = webhook.NewServer(webhook.Options{
+		Host:    c.WebhookServerHost,
+		Port:    c.WebhookServerPort,
+		CertDir: c.WebhookCertDir,
+	})
 }
 
 // Options initializes empty manager.Options, applies the set values and returns it.
