b94a9c4d0585d2b050c656e38aaf68945bbf964a
Ismail Alidzhikov <i.alidjikov@gmail.com>
2024-04-02 16:42:53 +0300

    Drop unit tests for AddToManager

--- pkg/input/input_data_service_test.go
+++ pkg/input/input_data_service_test.go
@@ -10,13 +10,8 @@ import (
 	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes/scheme"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 
 	"github.com/gardener/gardener-custom-metrics/pkg/input/input_data_registry"
-	"github.com/gardener/gardener-custom-metrics/pkg/input/metrics_scraper"
-	"github.com/gardener/gardener-custom-metrics/pkg/util/testutil"
 )
 
 var _ = Describe("input.inputDataService", func() {
@@ -72,93 +67,6 @@ var _ = Describe("input.inputDataService", func() {
 			kapis := result.GetShootKapis("ns")
 			Expect(kapis).To(HaveLen(1))
 			Expect(kapis[0].PodName()).To(Equal("pod"))
-		})
-	})
-
-	Describe("AddToManager", func() {
-		It("should add the scraper, pod controller, and secret controller to the manager", func() {
-			// Arrange
-			ids, _ := newInputDataService()
-			fm := testutil.NewFakeManager()
-
-			// Act
-			result := ids.AddToManager(fm)
-
-			// Assert
-			Expect(result).Should(Succeed())
-			Expect(testutil.GetRunnables[*metrics_scraper.Scraper](fm)).To(HaveLen(1))
-			Expect(testutil.GetRunnables[controller.Controller](fm)).To(HaveLen(2))
-		})
-
-		It("should add apimachinery runtime types scheme to the manager", func() {
-			// Arrange
-			ids, _ := newInputDataService()
-			fm := testutil.NewFakeManager()
-
-			// Act
-			result := ids.AddToManager(fm)
-
-			// Assert
-			Expect(result).To(Succeed())
-			runtimeScheme := runtime.NewScheme()
-			builder := runtime.NewSchemeBuilder(scheme.AddToScheme)
-			builder.AddToScheme(runtimeScheme)
-			for gvk := range runtimeScheme.AllKnownTypes() {
-				Expect(fm.Scheme.Recognizes(gvk)).To(BeTrue())
-			}
-		})
-
-		It("should create a new data registry and pass it to the scraper", func() {
-			// Arrange
-			ids, _ := newInputDataService()
-			fm := testutil.NewFakeManager()
-			var registryPassedToScraperConstructor input_data_registry.InputDataRegistry
-			ids.testIsolation.NewScraper = func(
-				dataRegistry input_data_registry.InputDataRegistry,
-				_ time.Duration,
-				_ time.Duration,
-				_ logr.Logger) *metrics_scraper.Scraper {
-
-				registryPassedToScraperConstructor = dataRegistry
-
-				return nil
-			}
-
-			// Act
-			result := ids.AddToManager(fm)
-
-			// Assert
-			Expect(result).To(Succeed())
-			Expect(registryPassedToScraperConstructor).NotTo(BeNil())
-			Expect(registryPassedToScraperConstructor == ids.inputDataRegistry).To(BeTrue())
-		})
-
-		It("should configure the scraper with the specified scrape period and flow control period", func() {
-			// Arrange
-			ids, _ := newInputDataService()
-			fm := testutil.NewFakeManager()
-			var scrapePeriodPassedToScraperConstructor time.Duration
-			var scrapeFlowControlPeriodPassedToScraperConstructor time.Duration
-
-			ids.testIsolation.NewScraper = func(
-				_ input_data_registry.InputDataRegistry,
-				scrapePeriod time.Duration,
-				scrapeFlowControlPeriod time.Duration,
-				_ logr.Logger) *metrics_scraper.Scraper {
-
-				scrapePeriodPassedToScraperConstructor = scrapePeriod
-				scrapeFlowControlPeriodPassedToScraperConstructor = scrapeFlowControlPeriod
-
-				return nil
-			}
-
-			// Act
-			result := ids.AddToManager(fm)
-
-			// Assert
-			Expect(result).To(Succeed())
-			Expect(scrapePeriodPassedToScraperConstructor).To(Equal(testScrapePeriod))
-			Expect(scrapeFlowControlPeriodPassedToScraperConstructor).To(Equal(testScrapeFlowControlPeriod))
 		})
 	})
 })
